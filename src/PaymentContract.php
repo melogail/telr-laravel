9386a104da98f5f82411709fb33c599639ee918b
melogail <melogail@users.noreply.github.com>
2023-03-19 17:52:59 +0000

    Fix styling

--- src/PaymentContract.php
+++ src/PaymentContract.php
@@ -7,7 +7,6 @@ interface PaymentContract
     /**
      * Set the amount of payment for each transaction.
      *
-     * @param $amount
      * @return mixed
      */
     public function setAmount($amount);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Melogail\TelrLaravel\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Melogail\TelrLaravel\SkeletonServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
