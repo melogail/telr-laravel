3964f0ba549d6e1689aa1d02436dc519f60c97c9
Mohamed Elogail <melogail@Mohameds-MacBook-Pro.local>
2023-03-20 15:04:28 +0200

    Publishing migrations with service provider

--- src/TelrLaravelServiceProvider.php
+++ src/TelrLaravelServiceProvider.php
@@ -25,12 +25,25 @@ class TelrLaravelServiceProvider extends PackageServiceProvider
 
     public function boot()
     {
+        // Publishing config file
         if ($this->app->runningInConsole()) {
+            // Exporting config file
             $this->publishes(
                 [
                     __DIR__.'/../config/telr-laravel.php' => config_path('telr-laravel.php'),
                 ], 'config'
             );
+
+            // Exporting migration files
+            $this->publishes(
+                [
+                    __DIR__ . '/../database/migrations/create_telr_transactions_table.php.stub' => database_path('migrations/' . date('Y_m_dHis', time()). 'create_telr_transactions_table'),
+                    // Add more migrations here.
+                ], 'migrations'
+            );
         }
+
+        // Auto publishing migration file
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
